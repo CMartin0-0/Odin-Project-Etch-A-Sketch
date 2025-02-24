c33e00964d48e75ac15cb96cb68ecc82a23cd88a
Caleb Martin <calebmartin2020@gmail.com>
2025-02-24 16:03:30 -0600

    publish first working version of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,94 @@
-
-
+import { ReactNode, useState } from 'react';
 import './App.css';
 
-function App() {
- 
+function GridContainer({ children }: { children: ReactNode })
+{
+    return <div className="container">{ children }</div>;
+}
 
-  return (
-    <>
-     <body>
-      {/*<!--nav start-->*/}
-      <nav id="navbar" className="nav">
-        <ul className="nav-list">
-          <li>
-           <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#welcome-section">Home</a>
-            </li>
-          </ul>  
-      </nav>
-      {/*<!--nav end-->*/}
-      {/*<!--welcome start-->*/}
-      <section id="welcome-section" className="welcome-section">
-        <h1>Caleb Martin</h1>
-        <p>an aspiring developer</p>
-        <div className="contacts-links">
-    <a id="profile-link" className="btn contact-details" href="https://github.com/CMartin0-0" target="_blank" rel="noreferrer">
-      <i className="fab fa-github">
-      </i>
-      Github
-      </a>
-      <a className="btn contact-details"
-      href="calebmartin2020@gmail.com">
-      <i className="fas fa-at">
-      </i>
-      Email
-      </a>
+function App()
+{
+    const [gridSize, setGridSize] = useState(0);
+    const grid = [];
+    const gridSquare = document.querySelectorAll<HTMLElement>('.container__row');
+    let trigger= false;
+    document.addEventListener('mousedown', function(){
+        trigger= true;
+    });
+    document.addEventListener('mouseup', function(){
+        trigger= false;
+    });
+
+    const handleClick = () =>
+    {
+        if (gridSquare) {
+            gridSquare.forEach((el) => el.style.setProperty('--grid_opacity', '0'));
+        }
+
+        const getGridSize = parseInt(
+            prompt('Enter size of new grid. Can be a number between 1 - 100.', '16')!
+        );
+        setGridSize(getGridSize);
+
+        if (getGridSize > 100 || getGridSize <= 0 || isNaN(getGridSize)) {
+            alert('Must be a number between 1 - 100');
+        }
+    }
+    const handleReset = () =>
+    {
+        if (gridSquare) {
+            gridSquare.forEach((el) => el.style.setProperty('--grid_opacity', '0'));
+        }
+    }
+
+    const handleMouseEnter = (e: React.MouseEvent<HTMLElement> ) =>
+    {
+        e.preventDefault();
+        const currOpacity = parseFloat(
+            window.getComputedStyle(e.currentTarget).getPropertyValue('--grid_opacity')
+            );
+        if (trigger === true) {
+            const opacityPlus = Math.min(currOpacity + 0.1, 1);
+            e.currentTarget.style.setProperty('--grid_opacity', opacityPlus.toString());
+        } else if (trigger === false) {
+            const opacityMinus = Math.max(currOpacity - 0.1, 0);
+            e.currentTarget.style.setProperty('--grid_opacity', opacityMinus.toString());
+        }
+    };
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        grid.push(
+            <div
+                className="container__row"
+                key={ i }
+                onMouseEnter={ handleMouseEnter }
+                style={ { width: 100 / gridSize + '%' } }
+                onDragStart={(e) => {e.preventDefault()}}
+            ></div>
+        );
+    }
+
+    return (
+        <div>
+            <p className='title'>Etch A Sketch</p>
+            <p className='instruction'>To use, first pick a grid size, then hold down mouse button and move cursor to start drawing. <br></br>
+                (to use the cursor as an eraser instead, just release the mouse button)</p>
+            <div className="container-two">
+                <GridContainer>{ grid }</GridContainer>
+            </div>
+            <div className='btn-wrapper'> 
+            <div className='btn-container'>
+            <button className="size btn" onClick={ handleClick }>
+                Grid Size
+            </button>
+            <button className="clear btn" onClick={ handleReset }>
+                Shake
+            </button>
+            </div>
+            </div> 
+            <p className='credit'>This project is part of theOdinProject curriculum.</p>
         </div>
-      </section>
-      {/*<!--welcome end-->*/}
-      {/*<!--projects start-->*/}
-      <section id="projects" className="projects-section">
-        <h2 className="projects-section-header">Projects</h2>
-        <a href="https://codepen.io/Caleb-Martin/full/gOZJjBK" className="project-tile">
-        <img className="project-image" src="https://shots.codepen.io/username/pen/gOZJjBK-800.jpg?version=1697428932" alt="project"></img>
-        <p className="project-title">Tribute Page</p>
-        </a>
-        <a href="https://codepen.io/Caleb-Martin/full/dyrpLmr" className="project-tile">
-       <img className="project-image" src="https://shots.codepen.io/username/pen/dyrpLmr-800.jpg?version=1705197082" alt="project"></img>
-       <p className="project-title">Palindrome Checker</p>
-        </a>
-        <a href="https://codepen.io/Caleb-Martin/pen/gOERLmq" className="project-tile">
-        <img className="project-image" src="https://shots.codepen.io/username/pen/gOERLmq-800.jpg?version=1705861333" alt="project"></img>
-        <p className="project-title">Roman Numeral Converter</p>
-        </a>
-      </section>
-      {/*<!--projects end-->*/}
-    </body>
-    </>
-  );
+    );
 }
 
 export default App;
